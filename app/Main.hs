73296d4f9c52bbe405e7ee2c8ad04393b6473b36
sylambdacode <syemailsyemail@163.com>
2025-08-24 04:34:12 -0400

    Add some tools

--- app/Main.hs
+++ app/Main.hs
@@ -10,22 +10,20 @@ import LambdaParser
 import Data.Map (Map)
 import qualified Data.Map as Map
 
-testCode :: String
-testCode = """
-        plus = λm n f x.m f (n f x);
-        3 = (λf x.f (f (f x)));
-        2 = (λf x.f (f x));
-
-        {- plus 2 3 = λf x.f (f (f (f (f x)))) -}
-        main = plus 2 3;
-    """
+import OtherTools
 
 main :: IO ()
-main = case parseCode "(test)" testCode of
-    Right result -> let valDefMap = valDefListToMap result
-        in case (Map.lookup "main" valDefMap) of
-            Just v -> print $ calculateNormalResult $ toLambdaTerm valDefMap v
-            Nothing -> print "not found main"
-    Left e -> do
-        putStrLn "parser error:"
-        putStrLn $ show e
+main = do
+    testCode <- getContents
+    case parseCode "(test)" testCode of
+        Right result -> let valDefMap = valDefListToMap result
+            in case (Map.lookup "main" valDefMap) of
+                Just v -> do
+                              putStrLn "开始运行"
+                              putStrLn $ show $ toLambdaTerm valDefMap v
+                              putStrLn "---------------------------------------------------------------------------------"
+                              print $ toBoolList $ calculateNormalResult $ toLambdaTerm valDefMap v
+                Nothing -> print "not found main"
+        Left e -> do
+            putStrLn "parser error:"
+            putStrLn $ show e
---
+++ app/OtherTools.hs
@@ -0,0 +1,45 @@
+module OtherTools where
+
+import LambdaTerm
+import LambdaReduction
+
+import LambdaParser
+
+import Data.Map (Map)
+import qualified Data.Map as Map
+
+nilLambdaTerm :: LambdaTerm
+nilLambdaTerm = Abstraction "n" (Abstraction "c" (Variable "c"))
+
+isNil :: LambdaTerm -> Bool
+isNil lambdaTerm = alphaEq Map.empty lambdaTerm nilLambdaTerm
+
+headLambdaTerm :: LambdaTerm
+headLambdaTerm = Abstraction "x" (Abstraction "xs" (Variable "x"))
+listHead :: LambdaTerm -> LambdaTerm
+listHead lambdaTerm = Application lambdaTerm headLambdaTerm
+
+tailLambdaTerm :: LambdaTerm
+tailLambdaTerm = Abstraction "x" (Abstraction "xs" (Variable "xs"))
+listTail :: LambdaTerm -> LambdaTerm
+listTail lambdaTerm = Application lambdaTerm tailLambdaTerm
+
+
+toInt' :: LambdaTerm -> Int
+toInt' (Variable _) = 0
+toInt' (Application (Variable _) body) = 1 + (toInt' body)
+toInt' _ = error "Not Number"
+
+toInt :: LambdaTerm -> Int
+toInt (Abstraction _ (Abstraction _ body)) = toInt' body
+toInt _ = error "Not Number"
+
+toBool :: LambdaTerm -> Bool
+toBool (Abstraction x (Abstraction y (Variable c))) = x == c
+toBool _ = error "Not Bool"
+
+toBoolList :: LambdaTerm -> [Bool]
+toBoolList lambdaTerm =
+    if isNil lambdaTerm
+    then []
+    else (toBool $ calculateNormalResult $ listHead $ lambdaTerm) : (toBoolList (calculateNormalResult $ listTail lambdaTerm))
