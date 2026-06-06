822365ef6457fa5eede54ddd65858f9f80cf59b1
sylambdacode <sy16367@163.com>
2026-06-06 16:16:07 +0800

    实现WHNF规约

--- app/UntypedLambdaCalculus/LambdaReduction.hs
+++ app/UntypedLambdaCalculus/LambdaReduction.hs
@@ -16,10 +16,28 @@ normalOrderBetaReduce (Application functionLambdaTerm argumentLambdaTerm) =
         else Application functionLambdaTermResult argumentLambdaTerm
     where functionLambdaTermResult = normalOrderBetaReduce functionLambdaTerm
 
+isWeakNormalHeadForm :: LambdaTerm -> Bool
+isWeakNormalHeadForm (Variable _) = True
+isWeakNormalHeadForm (Abstraction _ _) = True
+isWeakNormalHeadForm (Application (Abstraction _ _) _) = False
+isWeakNormalHeadForm (Application functionLambdaTerm _) = isWeakNormalHeadForm functionLambdaTerm
+
+calculateWeakNormalHeadResult :: LambdaTerm -> LambdaTerm
+calculateWeakNormalHeadResult lambdaTerm =
+    if isWeakNormalHeadForm lambdaTerm
+        then lambdaTerm
+        else calculateWeakNormalHeadResult newLambdaTerm
+    where newLambdaTerm = normalOrderBetaReduce lambdaTerm
+
+isNormalForm :: LambdaTerm -> Bool
+isNormalForm (Variable _) = True
+isNormalForm (Abstraction _ bodyLambdaTerm) = isNormalForm bodyLambdaTerm
+isNormalForm (Application (Abstraction _ _) _) = False
+isNormalForm (Application functionLambdaTerm argumentLambdaTerm) = isNormalForm functionLambdaTerm && isNormalForm argumentLambdaTerm
 
 calculateNormalResult :: LambdaTerm -> LambdaTerm
 calculateNormalResult lambdaTerm =
-    if alphaEq Map.empty lambdaTerm newLambdaTerm
-        then newLambdaTerm
+    if isNormalForm lambdaTerm
+        then lambdaTerm
         else calculateNormalResult newLambdaTerm
     where newLambdaTerm = normalOrderBetaReduce lambdaTerm
